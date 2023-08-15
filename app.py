289a97c4b4635c9cfe100fbc21f02eca7e9ac6a8
kevoyuan <kevinsyuan@gmail.com>
2023-08-15 22:17:24 +0200

    prompt

--- app.py
+++ app.py
@@ -12,21 +12,36 @@ with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
+
+
 def extract_properties_from_resume(resume_text):
+    # Modified prompt for the OpenAI API
+    prompt = '''
+    Extract the following properties from the resume,don't miss out on any property below:
+    - Name
+    - Contact details including: Email, GitHub, Birthdate, Birthplace, LinkedIn, Address, Phone
+    - Summary
+    - List of Education experiences including: Duration, Institution, Location, Degree, Focus
+    - List of Work Experiences including: Duration, Role, Location, Company, Responsibilities, Skills
+    - List of Project Experiences including: Duration, Title, Location, Institution, Topic, Grade, Responsibilities, Skills
+    - Languages & IT Skills: Break down the skills into categories. For the "Languages" category, list both the language and its proficiency. For other categories, list the skills under each category.
+    - List of Hobbies
+    - Location
+    - Date of last update
+    '''
     # Use the OpenAI API to process the resume
     response = openai.Completion.create(
         engine="davinci",
-        prompt=f"Extract properties from the following resume: {resume_text}",
-        max_tokens=500
+        prompt=f"{prompt}\n\n{resume_text}",
+        max_tokens=1000
     )
-
     # Extracted properties
     properties = response.choices[0].text.strip()
-
     # Convert the properties to a dictionary
     properties_dict = eval(properties)
-    
     return properties_dict
+
+
 
 def read_pdf(file):
     pdf = PdfFileReader(file)
